27d9ea1eedf5f909860dee3ee655727e2d0c06d2
Mihindu Wijesena <mihinduwijesena@gmail.com>
2021-08-08 15:20:28 +0530

    initial Commit

--- app/src/main/java/com/example/coronainformation/MainActivity.java
+++ app/src/main/java/com/example/coronainformation/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.coronainformation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +16,9 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +43,21 @@ public class MainActivity extends AppCompatActivity {
         final TextView lblGTotalRecovered = (TextView) findViewById(R.id.lblGTotRecovered);
         final TextView lblGNewCases = (TextView) findViewById(R.id.lblGNewCases);
         final TextView lblGTotCases = (TextView) findViewById(R.id.lblGTotCases);
+        final TextView lblUpdatedAt = (TextView) findViewById(R.id.lblUpdatedAt);
+        final TextView lblVersion = (TextView) findViewById(R.id.lblVersion);
+        final TextView lblDeveloper = (TextView) findViewById(R.id.lblDeveloper);
+        final TextView lblSource = (TextView)  findViewById(R.id.lblSource);
 
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(this.getPackageName(), 0);
+
+            lblVersion.setText("Corona Information : " + pInfo.versionName);
+            lblDeveloper.setText("Developer :  Mihindu Wijesena" );
+            lblSource.setText("Source : https://www.hpb.health.gov.lk");
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
 
 
@@ -55,18 +74,19 @@ public class MainActivity extends AppCompatActivity {
                               //  JSONArray jsonArray   = response.getJSONArray("data");
                                 JSONObject jsonObject = response.getJSONObject("data");
                                 Toast.makeText(getApplicationContext(),jsonObject.getString("local_new_cases"),Toast.LENGTH_LONG).show();
-                                lblNewCases.setText(jsonObject.getString("local_new_cases"));
-                                lblTotalCases.setText(jsonObject.getString("local_total_cases"));
-                                lblTotalDeaths.setText(jsonObject.getString("local_deaths"));
-                                lblNewDeaths.setText(jsonObject.getString("local_new_deaths"));
-                                lblTotInvInHost.setText(jsonObject.getString("local_total_number_of_individuals_in_hospitals"));
-                                lblActiveCases.setText(jsonObject.getString("local_active_cases"));
-                                lblTotalRecovered.setText(jsonObject.getString("local_recovered"));
+                                lblNewCases.setText(formatNumber(jsonObject.getString("local_new_cases")));
+                                lblTotalCases.setText(formatNumber(jsonObject.getString("local_total_cases")));
+                                lblTotalDeaths.setText(formatNumber(jsonObject.getString("local_deaths")));
+                                lblNewDeaths.setText(formatNumber(jsonObject.getString("local_new_deaths")));
+                                lblTotInvInHost.setText(formatNumber(jsonObject.getString("local_total_number_of_individuals_in_hospitals")));
+                                lblActiveCases.setText(formatNumber(jsonObject.getString("local_active_cases")));
+                                lblTotalRecovered.setText(formatNumber(jsonObject.getString("local_recovered")));
 
-                                lblGDeaths.setText(jsonObject.getString("global_deaths"));
-                                lblGTotalRecovered.setText(jsonObject.getString("global_recovered"));
-                                lblGNewCases.setText(jsonObject.getString("global_new_cases"));
-                                lblGTotCases.setText(jsonObject.getString("global_total_cases"));
+                                lblGDeaths.setText(formatNumber(jsonObject.getString("global_deaths")));
+                                lblGTotalRecovered.setText(formatNumber(jsonObject.getString("global_recovered")));
+                                lblGNewCases.setText(formatNumber(jsonObject.getString("global_new_cases")));
+                                lblGTotCases.setText(formatNumber(jsonObject.getString("global_total_cases")));
+                                lblUpdatedAt.setText("Updated At : " + jsonObject.getString("update_date_time"));
 
 
                             } catch (Exception e) {
@@ -90,5 +110,12 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private String formatNumber(String value) {
+        String strTemp;
+        int temp = Integer.parseInt(value);
+        strTemp = NumberFormat.getInstance().format(temp);
+        return strTemp;
     }
 }
